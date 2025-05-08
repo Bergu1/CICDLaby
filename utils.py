5bcb299924f8290bf97a5c2f7dab99b16c34c974
Konrad Landzberg <konrad-landzberg@wp.pl>
2025-05-08 09:41:55 +0200

    commit changes

--- utils.py
+++ utils.py
@@ -1,13 +1,17 @@
 # przykładowe funkcje utils.py
 
+
 def add(a: int, b: int) -> int:
     return a + b
+
 
 def subtract(a: int, b: int) -> int:
     return a - b
 
+
 def multiply(a: int, b: int) -> int:
     return a * b
+
 
 def divide(a: int, b: int) -> float:
     return a / b
